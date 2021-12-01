f46eaf0d48548a262ae4398c5f48a968396d9d86
Devil7DK <dineshthangavel47@gmail.com>
2021-12-02 00:35:15 +0530

    Remove node server

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+/**
+ * @typedef {object} Product
+ * @property {number} id ID of the product
+ * @property {string} product_name Name of the product
+ * @property {number} price Price of the product
+ */
+
+/**
+ * @typedef {object} ProductsResponse
+ * @property {Array<Product>} items List of products
+ * @property {number} total Total number of products
+ */
+
+/**
+ * @typedef {object} CartItem
+ * @property {Product} product Product linked with cart item
+ * @property {number} count Cart item count
+ */
+
+/**
+ * @typedef {Map<number, CartItem>} CartItems
+ */
+
 $(function () {
     /**
      * @type {CartItems}
